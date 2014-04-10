d10d9118c75d33fb9583ffdc57bfc2beec55a9a1
Kay Strobach <github@kay-strobach.de>
2014-04-10 15:29:28 +0200

    init

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'BE Layouts by TypoScript',
+	'description' => 'Provider for having BE layouts in PageTS',
+	'category' => 'be',
+	'author' => 'Kay Strobach',
+	'author_email' => 'typo3@kay-strobach.de',
+	'author_company' => 'www.kay-strobach.de',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+);
